11fd958f29a551e133023a6ab5396b3e1ebf838d
ae-chae <aechae11@gmail.com>
2024-09-09 21:48:17 +0900

    [Add] Setting

---
+++ src/test/java/com/example/wanted3/Wanted3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanted3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wanted3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
